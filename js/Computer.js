6bc33e40ed462a6ce5a4f958c782053cd3610996
Guneet410 <85295632+Guneet410@users.noreply.github.com>
2021-06-21 18:02:21 +0530

    Add files via upload

---
+++ js/Computer.js
@@ -0,0 +1,43 @@
+class Computer {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+  
+
+    World.add(world, this.body);
+  }
+
+ 
+  
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  
+
+    rectMode(CENTER)
+     push();
+     textSize (20)
+     fill("white")
+     text("Computer",width-250, height-height+30)
+     fill ("green");
+     rect(width-350,height-height+70,80,50)
+     rect(width-250,height-height+70,80,50)
+     rect(width-150,height-height+70,80,50)
+     pop()
+  
+  }
+}
